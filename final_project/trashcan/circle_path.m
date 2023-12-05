566ce62b7d2398836811ee07b6674a0b0dca71d6
HAN2496 <137390470+HAN2496@users.noreply.github.com>
2023-12-05 17:18:04 +0900

    1

--- final_project/trashcan/circle_path.m
+++ final_project/trashcan/circle_path.m
@@ -1,4 +1,3 @@
-delt=0.0001;
 
 nowx = 0;   nowy = 0;   %현재위치
 tarx = 40;   tary = 30; %목표위치
@@ -12,12 +11,13 @@ vTarget = 4;
 r = sqrt((dx)^2+(dy)^2)/2;
 path_len = pi*r;
 t0 = path_len/vTarget;
-theta = atan((dy)/(x0-x1));
+theta = atan((dy)/(dx));
 
 t = (0:0.001:t0)';
 vx = -2*r*sin(theta+t*pi/t0)*pi/t0;  vx(end+1:end+2000)=zeros(2000,1);
 vy = 2*r*cos(theta+t*pi/t0)*pi/t0; vy(end+1:end+2000)=zeros(2000,1);
 
+%이동후 멈추게 하려고 t0+2초 해줌 (2초동안 속도0)
 t = (0:0.001:t0+2)';
 plot(vx,vy)
 tsec = seconds(t);
---
+++ final_project/trashcan/velocity_control.m
@@ -0,0 +1,48 @@
+% 1회전당 2mm 이동
+% 모터 최고 속도는 6.25 회전/s
+timeStep = 0.001;
+t0 = 5;   %가속구간 길이
+
+x1 = 1;
+y1 = 6;
+targetV = 3;
+
+targetX = x1*2;
+targetY = y1*2;
+targetVx = targetX/sqrt(targetX.^2 + targetY.^2) * targetV;
+targetVy = targetY/sqrt(targetX.^2 + targetY.^2) * targetV;
+
+
+sum_term = 0;
+for i=1:timeStep:t0
+    sum_term = sum_term + sineprofile(targetVx, t0, i) * timeStep;
+end
+totalTime = (targetX - sum_term) / targetVx + 2 * t0;
+
+timePoints = 0:0.001:totalTime;
+accel_len = t0 * 1000 + 1;
+time_len = length(timePoints);
+velocityProfile=zeros(time_len ,2);
+
+
+for idx=1:time_len
+    t = timePoints(idx);
+    if idx <= accel_len
+        velocityProfile(idx, 1) = sineprofile(targetVx, t0, t);
+        velocityProfile(idx, 2) = velocityProfile(idx, 1) * targetVy/targetVx;
+    elseif idx <= time_len - accel_len
+        velocityProfile(idx, 1) = targetVx;
+        velocityProfile(idx, 2) = targetVy;
+    else
+        velocityProfile(idx, 1) = sineprofile(targetVx, t0, totalTime-t);
+        velocityProfile(idx, 2) = velocityProfile(idx, 1) * targetVy/targetVx;
+    end
+end
+t=seconds(timePoints');
+xvelinput=timetable(t,velocityProfile(:,1));
+yvelinput=timetable(t,velocityProfile(:,2));
+
+% 데이터 확인용 플롯
+plot(timePoints, velocityProfile(:, 1))
+hold on
+plot(timePoints, velocityProfile(:, 2))
